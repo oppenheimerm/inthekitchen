b8e620b5531aeae9ff091c0df30f63ee55d85de5
michieloppenheimer <michieloppenheimer@gmail.com>
2022-12-15 18:47:18 +0000

    home section initial

--- ITK.Web/wwwroot/js/site.js
+++ ITK.Web/wwwroot/js/site.js
@@ -1,4 +1,68 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*=============== SHOW MENU ===============*/
 
-// Write your JavaScript code.
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW CART ===============*/
+
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW LOGIN ===============*/
+
+
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+window.addEventListener('scroll', function (e) {
+    let header = document.getElementById('header');
+    if (this.window.scrollY >= 90) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+    //Only notify every 500 ms
+}, 500);
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== LIGHT BOX ===============*/
+
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+
+/*=============== STYLE SWITCHER ===============*/
